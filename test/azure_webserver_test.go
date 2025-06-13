97842464ee95a80f5a6688bb5d58cb2fd7816ca5
MichaelBalcerzak <balcerzak.michael@gmail.com>
2025-06-13 11:43:06 -0400

    Added two new test

--- test/azure_webserver_test.go
+++ test/azure_webserver_test.go
@@ -6,11 +6,12 @@ import (
 	"github.com/gruntwork-io/terratest/modules/azure"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
+	//"github.com/stretchr/testify/require"
 )
 
 // You normally want to run this under a separate "Testing" subscription
 // For lab purposes you will use your assigned subscription under the Cloud Dev/Ops program tenant
-var subscriptionID string = "<your-azure-subscription-id"
+var subscriptionID string = "3b36c431-a92e-4ac8-927b-17a0f4b30054"
 
 func TestAzureLinuxVMCreation(t *testing.T) {
 	terraformOptions := &terraform.Options{
@@ -34,3 +35,50 @@ func TestAzureLinuxVMCreation(t *testing.T) {
 	// Confirm VM exists
 	assert.True(t, azure.VirtualMachineExists(t, vmName, resourceGroupName, subscriptionID))
 }
+
+// GetVirtualMachineNics checks if the NIC exists in the VM.
+// This function would fail the test if there is an error.
+//func GetVirtualMachineNics(t *testing.T, vmName string, resGroupName string, subscriptionID string) []string {
+//	nicList, err := azure.GetVirtualMachineNicsE(vmName, resGroupName, subscriptionID)
+//	require.NoError(err)
+
+//	return nicList
+//}
+
+// GetVirtualMachineNicsE checks if the VM runs the correct Ubuntu version.
+/*func GetVirtualMachineNicsE(vmName string, resGroupName string, subscriptionID string) ([]string, error) {
+
+	// Get VM Object
+	vm, err := azure.GetVirtualMachineE(vmName, resGroupName, subscriptionID)
+	if err != nil {
+		return nil, err
+	}
+
+	// Get VM NIC(s); value always present, no nil checks needed.
+	vmNICs := *vm.NetworkProfile.NetworkInterfaces
+
+	nics := make([]string, len(vmNICs))
+	for i, nic := range vmNICs {
+		// Get ID from resource string.
+		nicName := azure.GetNameFromResourceID(*nic.ID)
+		if err == nil {
+			nics[i] = nicName
+		}
+	}
+	return nics, nil
+}
+
+// ComfirmIfVMRunningAndCorrectUbuntu checks if the VM runs the correct Ubuntu version.
+func ComfirmIfVMRunningAndCorrectUbuntu(t *testing.T, vmName string, resGroupName string, subscriptionID string) ([]string, error) {
+	// Get VM Object
+	vm, err := azure.GetVirtualMachineE(vmName, resGroupName, subscriptionID)
+	if err != nil {
+		return nil, err
+	}
+
+	require.Equal(t, "Canonical", *vm.StorageProfile.ImageReference.Publisher)
+    require.Equal(t, "UbuntuServer", *vm.StorageProfile.ImageReference.Offer)
+    require.Equal(t, "18.04-LTS", *vm.StorageProfile.ImageReference.Sku) // Adjust as needed
+
+	return nil, err
+}*/
